8488e0e926d1a7eacad54e4cfb12259fcdc9ace0
Hassan <hharb@anl.gov>
2024-06-03 09:35:22 -0500

    add catalysis

---
+++ file_search_openai_cat.py
@@ -0,0 +1,124 @@
+from openai import OpenAI
+import os 
+ 
+client = OpenAI()
+ 
+assistant = client.beta.assistants.create(
+  name="Catalysis Assistant",
+#  instructions="You are an expert electrochemist. You provide well constructed scientific paragraphs on a given topic. Your expertise is in iron redox chemistry", # improve this instruction, add more details on chemistry
+  instructions="You are an expert chemist who specializes in foundation models for catalysis. You will be given questions and you will answer them from your expertise and from the pdf documents provided.",
+  model="gpt-4o",
+  tools=[{"type": "file_search"}],
+)
+
+
+# Create a vector store caled "Financial Statements"
+vector_store = client.beta.vector_stores.create(name="LOHC Data")
+ 
+# Ready the files for upload to OpenAI
+#file_paths = ["pdfs/1.pdf","pdfs/2.pdf","pdfs/3.pdf"]
+#file_paths = ["fe_pdfs/1.pdf","fe_pdfs/2.pdf","fe_pdfs/3.pdf","fe_pdfs/4.pdf","fe_pdfs/5.pdf"] # generalize this, provide a directory and have the code read all .pdf files
+#file_streams = [open(path, "rb") for path in file_paths]
+
+# Specify the directory containing the PDF files
+directory = "cat_pdf"
+
+# Get all .pdf files in the specified directory
+file_paths = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".pdf")]
+
+# Open the PDF files
+file_streams = [open(path, "rb") for path in file_paths]
+
+# Example usage
+print(file_paths)  # This will print the list of PDF file paths
+
+
+
+
+ 
+# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+# and poll the status of the file batch for completion.
+file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+  vector_store_id=vector_store.id, files=file_streams
+)
+ 
+# You can print the status and the file counts of the batch to see the result of this operation.
+print(file_batch.status)
+print(file_batch.file_counts)
+
+assistant = client.beta.assistants.update(
+  assistant_id=assistant.id,
+  tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+)
+
+# Upload the user provided file to OpenAI
+message_file = client.files.create(
+  file=open("fe_pdfs/1.pdf", "rb"), purpose="assistants"
+)
+
+content = input("Write your prompt here:\n") # user enters prompts when we run the code, here is where we will experiment with different prompts
+ 
+# Create a thread and attach the file to the message
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+#      "content": "Write a well structured paragraph on the importance of studying the redox potential of Fe ions, give examples. Provide the paragraph in a well structured scientific format.",
+#      "content": "Give me a full survey of the ions that can increase redox potential of iron(II). Give them in a well structured output",
+      "content": content,
+      # Attach the new file to the message.
+      "attachments": [
+        { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
+      ],
+    }
+  ]
+)
+ 
+# The thread now has a vector store with that file in its tool resources.
+print(thread.tool_resources.file_search)
+
+
+from typing_extensions import override
+from openai import AssistantEventHandler, OpenAI
+ 
+client = OpenAI()
+ 
+class EventHandler(AssistantEventHandler):
+    @override
+    def on_text_created(self, text) -> None:
+        print(f"\nassistant > ", end="", flush=True)
+
+    @override
+    def on_tool_call_created(self, tool_call):
+        print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+    @override
+    def on_message_done(self, message) -> None:
+        # print a citation to the file searched
+        message_content = message.content[0].text
+        annotations = message_content.annotations
+        citations = []
+        for index, annotation in enumerate(annotations):
+            message_content.value = message_content.value.replace(
+                annotation.text, f"[{index}]"
+            )
+            if file_citation := getattr(annotation, "file_citation", None):
+                cited_file = client.files.retrieve(file_citation.file_id)
+                citations.append(f"[{index}] {cited_file.filename}")
+
+        print(message_content.value)
+        print("\n".join(citations))
+
+
+# Then, we use the stream SDK helper
+# with the EventHandler class to create the Run
+# and stream the response.
+
+with client.beta.threads.runs.stream(
+    thread_id=thread.id,
+    assistant_id=assistant.id,
+    instructions="Please address the user as Jane Doe. The user has a premium account.",
+    event_handler=EventHandler(),
+) as stream:
+    stream.until_done()
+
